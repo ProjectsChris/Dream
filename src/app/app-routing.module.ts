ef762900be9bf35b2c551818d4a34b7e6f8b109d
ProjectsChris <christopher2002buffon@gmail.com>
2022-12-13 15:44:05 +0100

    Creazione delle pagine di accesso dell'utente (Login, Registration, Forgot Password)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./access-user/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./access-user/registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./access-user/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
   },
 ];
 
